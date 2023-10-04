0a41dfe8491406d6547d7eceaee27393b4bbf630
SabinaGracheva <grachevaqa@yandex.ru>
2023-10-05 01:07:12 +0300

    Добавлен файл locators.py

--- locators.py
+++ locators.py
@@ -2,18 +2,9 @@ from selenium.webdriver.common.by import By
 
 
 class Locators:
-    LOGIN_EMAIL = (By.XPATH, '//input[@name="name"]')
-    LOGIN_PASSWORD = (By.XPATH, '//input[@name="Пароль"]')
-    LOGIN_BUTTON_ON_THE_LOGIN_PAGE = (By.XPATH, "//button[text()='Войти']")
-    BUILD_A_BURGER = (By.XPATH, '//h1[text()="Соберите бургер"]')
-    LOGIN_BUTTON_ON_THE_MAIN_PAGE = (By.XPATH, "//button[text()='Войти в аккаунт']")
-    PERSONAL_ACCOUNT_BUTTON = (By.XPATH, '//a[@href="/account"]')
-
-
-
-
-    REG_BUTTON_IN_ENTRY = (By.XPATH, "//a[@href='/register']")
-    REG_BUTTON = (By.XPATH, "//button[text()='Зарегистрироваться']")
-    REG_INPUT = (By.XPATH, '//input[@class="text input__textfield text_type_main-default"]')
-
-    CONSTRUCTOR = (By.XPATH, '//a[@href="/"]')
+    LOGIN_EMAIL = (By.XPATH, '//input[@name="name"]')  # поле ввода email на странице авторизации
+    LOGIN_PASSWORD = (By.XPATH, '//input[@name="Пароль"]')  # поле ввода пароля на странице авторизации
+    LOGIN_BUTTON_ON_THE_LOGIN_PAGE = (By.XPATH, "//button[text()='Войти']")  # кнопка "Вход" на странице авторизации
+    BUILD_A_BURGER = (By.XPATH, '//h1[text()="Соберите бургер"]')  # тест "Соберите бургер" на главной странице
+    LOGIN_BUTTON_ON_THE_MAIN_PAGE = (By.XPATH, "//button[text()='Войти в аккаунт']")  # кнопка "Войти в аккаунт" на главной странице
+    PERSONAL_ACCOUNT_BUTTON = (By.XPATH, '//a[@href="/account"]')  # кнопка "Личный кабинет"
