9d754efd8ef6154dfba33bc60bbd99d3f90b69bc
SabinaGracheva <grachevaqa@yandex.ru>
2023-10-09 00:53:21 +0300

    Обновила файл с локаторами locators.py

--- locators.py
+++ locators.py
@@ -1,10 +1,31 @@
 from selenium.webdriver.common.by import By
 
 
-class Locators:
-    LOGIN_EMAIL = (By.XPATH, '//input[@name="name"]')  # поле ввода email на странице авторизации
-    LOGIN_PASSWORD = (By.XPATH, '//input[@name="Пароль"]')  # поле ввода пароля на странице авторизации
-    LOGIN_BUTTON_ON_THE_LOGIN_PAGE = (By.XPATH, "//button[text()='Войти']")  # кнопка "Вход" на странице авторизации
-    BUILD_A_BURGER = (By.XPATH, '//h1[text()="Соберите бургер"]')  # тест "Соберите бургер" на главной странице
-    LOGIN_BUTTON_ON_THE_MAIN_PAGE = (By.XPATH, "//button[text()='Войти в аккаунт']")  # кнопка "Войти в аккаунт" на главной странице
+class MainPageLocators:
+    # Локаторы главной страницы
+    LOGIN_BUTTON = (By.XPATH, "//button[text()='Войти в аккаунт']")  # кнопка "Войти в аккаунт" на главной странице
     PERSONAL_ACCOUNT_BUTTON = (By.XPATH, '//a[@href="/account"]')  # кнопка "Личный кабинет"
+    CONSTRUCTOR = (By.XPATH, '//a[@href="/"]')  # кнопка "Конструктор"
+    BUILD_A_BURGER = (By.XPATH, '//h1[text()="Соберите бургер"]')  # тест "Соберите бургер" на главной странице
+
+
+class AuthPageLocators:
+    # Локаторы страницы входа в аккаунт
+    EMAIL = (By.XPATH, '//input[@name="name"]')  # поле ввода email на странице авторизации
+    PASSWORD = (By.XPATH, '//input[@name="Пароль"]')  # поле ввода пароля на странице авторизации
+    LOGIN_BUTTON = (By.XPATH, "//button[text()='Войти']")  # кнопка "Войти"
+    REG_BUTTON_IN_ENTRY = (By.XPATH, '//a[@href="/register"]')  # текст-ссылка "Зарегистрироваться"
+    RESTORE_PASSWORD = (By.XPATH, '//a[@href="/forgot-password"]')  # текст-ссылка "Восстановить пароль"
+
+
+class RegPageLocators:
+    # Локаторы страницы регистрации
+    CREATE_NAME = (By.XPATH, '(//input[@name="name"])[1]')  # поле ввода имени
+    CREATE_EMAIL = (By.XPATH, '(//input[@name="name"])[2]')  # поле ввода email
+    CREATE_PASSWORD = (By.XPATH, '//input[@name="Пароль"]')  # поле ввода пароля
+    REG_BUTTON = (By.XPATH, '//button[text()="Зарегистрироваться"]')  # кнопка "Зарегистрироваться"
+
+
+class PersonalAccountPageLocators:
+    # Локаторы страницы "Личный кабинет"
+    LOGOUT_BUTTON = (By.XPATH, '//button[text()="Выход"]')  # кнопка выхода из Личного кабинета
